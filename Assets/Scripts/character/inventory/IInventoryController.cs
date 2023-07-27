36fe689c46f7a7c6926a71a5623a1aa36bcd10eb
Gusseth <17581117+Gusseth@users.noreply.github.com>
2023-07-27 04:23:02 -0700

    Add stubs for inventory module

---
+++ Assets/Scripts/character/inventory/IInventoryController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public interface IInventoryController
+{
+    public IInventory Inventory { get; }
+    public bool AddItem(Item item);
+    public bool AddItem(Item item, uint amount);
+    public bool RemoveItem(Item item);
+    public bool RemoveItem(Item item, uint amount);
+    public void TickInventory(Time deltaTime, IHaveInventory actor);
+    public Item[] FindItemsWithID(ItemID id);
+    public Item[] FindItemsWithAttributes(ItemData data);
+}
+
+public interface IInventory
+{
+    public bool AddItem(Item item, uint amount);
+    public bool RemoveItem(Item item, uint amount);
+    public Item[] FindItemsWithID(ItemID id);
+    public Item[] FindItemsWithAttributes(ItemData data);
+    public Item[] GetAllItems();
+}
---
+++ Assets/Scripts/character/inventory/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public abstract class Item
+{
+
+}
+
+public class ItemData
+{
+
+}
+
+public class ItemID
+{
+
+}
