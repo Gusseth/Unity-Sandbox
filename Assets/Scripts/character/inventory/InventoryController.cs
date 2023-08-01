7e7ad2a61283d965d67e4d9f5484545ef7f901f5
Gusseth <17581117+Gusseth@users.noreply.github.com>
2023-08-01 04:04:57 -0700

    Added basic hotbar

--- Assets/Scripts/character/inventory/InventoryController.cs
+++ Assets/Scripts/character/inventory/InventoryController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using static UnityEditor.Progress;
 
 public class InventoryController : MonoBehaviour, IInventoryController
 {
-    [SerializeField] GameObject[] quickEquip;
+    [SerializeField] List<ItemStack> equippedHotbar;
     [SerializeField] int i = 0;
     [SerializeField] SimpleInventory inventory;
+    [SerializeField] HotbarUI hotbarUI;
+    [SerializeField] TextMeshProUGUI hotbarNameIndicator;
 
     public IInventory Inventory => inventory;
 
@@ -38,25 +41,35 @@ public class InventoryController : MonoBehaviour, IInventoryController
 
     public GameObject GetEquipped(Hand hand, Transform parent)
     {
-        return Instantiate(quickEquip[i], parent);
+        return GetEquippedModel(parent);
+    }
+
+    private GameObject GetEquippedModel(Transform parent)
+    {
+        if (hotbarUI.Slots.Count == 0)
+        {
+            hotbarUI.SetHotbar(equippedHotbar);
+        }
+        hotbarUI.SelectSlot(i);
+        return Instantiate(equippedHotbar[i].worldModelPrefab, parent);
     }
 
     public GameObject GetNextEquipped(Hand hand, Transform parent)
     {
-        if (i == quickEquip.Length - 1)
+        if (i == equippedHotbar.Count - 1)
             i = 0;
         else
             i++;
-        return Instantiate(quickEquip[i], parent);
+        return GetEquippedModel(parent);
     }
 
     public GameObject GetPrevEquipped(Hand hand, Transform parent)
     {
         if (i == 0)
-            i = quickEquip.Length - 1;
+            i = equippedHotbar.Count - 1;
         else
             i--;
-        return Instantiate(quickEquip[i], parent);
+        return GetEquippedModel(parent);
     }
 
     public bool RemoveItem(ItemBase itemBase)
@@ -85,9 +98,9 @@ public class InventoryController : MonoBehaviour, IInventoryController
     }
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        hotbarUI.SetHotbar(equippedHotbar);
     }
 
     // Update is called once per frame
