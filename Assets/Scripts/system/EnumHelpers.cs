e05e0f3a9982b851e73f36e0d1a83261532219d7
Gusseth <17581117+Gusseth@users.noreply.github.com>
2023-07-31 21:18:03 -0700

    Added new utility helper functions + fixed inverted camera deltaVelocity

---
+++ Assets/Scripts/system/EnumHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EnumHelpers
+{
+    public static BasicHitDirection ToBasicHitDirection(Vector3 axis)
+    {
+        BasicHitDirection attackDirection;
+        if (axis.x != 0)
+        {
+            if (axis.x > 0)
+            {
+                attackDirection = BasicHitDirection.Right;
+            }
+            else
+            {
+                attackDirection = BasicHitDirection.Left;
+            }
+        }
+        else
+        {
+            if (axis.y > 0)
+            {
+                attackDirection = BasicHitDirection.Up;
+            }
+            else
+            {
+                attackDirection = BasicHitDirection.Down;
+            }
+        }
+
+        return attackDirection;
+    }
+}
+
