890fceb9ffe4c326dbb3d84fb2d299dc5773a617
Gusseth <17581117+Gusseth@users.noreply.github.com>
2023-08-24 01:21:06 -0700

    Added dummy caster for testing

---
+++ Assets/Scripts/actors/DummyCaster.cs
@@ -0,0 +1,57 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DummyCaster : MonoBehaviour
+{
+    [SerializeField] INoritoInventoryController inventoryController;
+    [SerializeField] int delayInMilliseconds = 1000;
+    [SerializeField] float castableSpeed = 5.0f;
+    [SerializeField] bool rotateCastablesAfterCast;
+    CancellationToken token;
+
+    private void Awake()
+    {
+        inventoryController = GetComponent<SimpleNoritoInventoryController>();
+        inventoryController.SetEquipped(0, transform);
+        token = this.GetCancellationTokenOnDestroy();
+    }
+
+    private void Start()
+    {
+        TimeHelpers.InvokeAsync(ShootRoutine, delayInMilliseconds, token);
+    }
+
+    private CastingData MakeCastData()
+    {
+        return new CastingData
+        {
+            origin = transform,
+            owner = gameObject,
+
+            speed = castableSpeed,
+            direction = transform.forward
+        };
+    }
+
+    private void Cast()
+    {
+        inventoryController.OnCast(MakeCastData());
+    }
+
+    void ShootRoutine()
+    {
+        Cast();
+        if (rotateCastablesAfterCast)
+        {
+            inventoryController.GetNextEquipped(transform);
+        }
+
+        TimeHelpers.InvokeAsync(ShootRoutine, delayInMilliseconds, token);
+    }
+}
---
+++ Assets/Scripts/character/inventory/norito/SimpleNoritoInventoryController.cs
@@ -0,0 +1,61 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+/*  List-based norito inventory system.
+ *  Very simple.
+ */
+[Serializable]
+public class SimpleNoritoInventoryController : MonoBehaviour, INoritoInventoryController
+{
+    [SerializeReference, SubclassSelector] List<ICastable> castables = new List<ICastable>();
+    ICastable current;
+    int i;
+    INoritoInventory INoritoInventoryController.NoritoInventory => null;
+
+    public GameObject GetCurrentEquipped(Transform parent)
+    {
+        return null;
+    }
+
+    public GameObject GetNextEquipped(Transform parent)
+    {
+        return SetEquipped((i + 1) % castables.Count, parent);
+    }
+
+    public GameObject GetPrevEquipped(Transform parent)
+    {
+        return SetEquipped((i - 1) % castables.Count, parent);
+    }
+
+    public GameObject SetEquipped(int i, Transform parent)
+    {
+        UpdateCurrent();
+        if (i >= castables.Count || i < 0)
+            return null;
+        this.i = i;
+        return GetCurrentEquipped(parent);
+    }
+
+    private void UpdateCurrent()
+    {
+        if (current != null)
+            current.OnEquipOut();
+    }
+
+    bool INoritoInventoryController.OnCast(CastingData castData)
+    {
+        current.OnCastAsync(castData, null, this.GetCancellationTokenOnDestroy());
+        return true;
+    }
+
+    void Awake()
+    {
+        current = castables[i];
+        foreach (ICastable castable in castables)
+            castable.CalculateKeCost();
+    }
+}
