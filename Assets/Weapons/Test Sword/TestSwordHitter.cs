599ddb42d1cd7a96b7f54a9224a3109f853be10a
Gusseth <17581117+Gusseth@users.noreply.github.com>
2023-08-31 22:17:32 -0700

    Moved TestSword

--- Assets/Weapons/Test Sword/TestSwordHitter.cs
+++ Assets/Weapons/Test Sword/TestSwordHitter.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class TestSwordHitter : AbstractMeleeHitter
+public class TestSwordHitter : AbstractDirectionalMeleeHitter
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/Weapons/Test Sword/TestSwordHitterBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class TestSwordHitterBox : AbstractMeleeHitterBox
+{
+    [SerializeField] GameObject spark;
+
+    protected override void OnBlockerHit(Block data)
+    {
+        base.OnBlockerHit(data);
+
+        Hitter.Attacking = false;
+        Debug.Log("I hit a HitterBox, meaning I got blocked :(");
+        GameObject s = Instantiate(spark);
+
+        s.transform.position = data.point + data.normal * .05f;
+        s.transform.Rotate(data.normal);
+
+        Root.Debug.DrawLight(s.transform.position);
+        Hitter.PostAttack();
+    }
+}
